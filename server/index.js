101f78b2df730af418332cc7435499c25e118cce
Aanyas24 <aanyasukhija@gmail.com>
2026-06-02 16:40:11 +0530

    Configured port for deployment

--- server/index.js
+++ server/index.js
@@ -14,6 +14,8 @@ app.get("/", (req, res) => {
 
 app.use("/api/github", githubRoutes);
 
-app.listen(5000, () => {
-  console.log("Server Started");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
